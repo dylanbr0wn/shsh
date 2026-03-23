61eb3e2f03866281000b608a6ac95d5d429ea2d4
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2026-03-22 21:08:43 -0700

    fixes

--- frontend/src/components/ui/input-group.tsx
+++ frontend/src/components/ui/input-group.tsx
@@ -48,7 +48,7 @@ function InputGroupAddon({
   ...props
 }: React.ComponentProps<'div'> & VariantProps<typeof inputGroupAddonVariants>) {
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events
     <div
       role="group"
       data-slot="input-group-addon"
