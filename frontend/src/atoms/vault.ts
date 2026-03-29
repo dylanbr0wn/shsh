6753e52152ef830433511d0185c88111c9cb2163
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2026-03-28 19:40:19 -0700

    feat(ui): add vault jotai atoms

---
+++ frontend/src/atoms/vault.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+export const vaultLockedAtom = atom<boolean>(false);
+export const vaultEnabledAtom = atom<boolean>(false);
+export const biometricAvailableAtom = atom<boolean>(false);
